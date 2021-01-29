6559e6505de87a579535da331736d1cfe646ba88
Piotr Sikora <psikora@inelo.pl>
2021-01-29 16:20:47 +0100

    Zmiana min temperatury dla nozzle na 15C

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -451,7 +451,7 @@
 
 // Below this temperature the heater will be switched off
 // because it probably indicates a broken thermistor wire.
-#define HEATER_0_MINTEMP   21
+#define HEATER_0_MINTEMP   15
 #define HEATER_1_MINTEMP   5
 #define HEATER_2_MINTEMP   5
 #define HEATER_3_MINTEMP   5
