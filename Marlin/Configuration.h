1a6fe93ddf132badbdc550d778e0c3f4abc83f23
Piotr Sikora <psikora@inelo.pl>
2021-02-18 12:50:39 +0100

    Finalna kalibracja ekstrudera (Trianglelabs BMG) - 420mm/step

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -740,7 +740,7 @@
  * Override with M92
  *                                      X, Y, Z, E0 [, E1[, E2...]]
  */
-#define DEFAULT_AXIS_STEPS_PER_UNIT   { 100, 100, 400, 430 }
+#define DEFAULT_AXIS_STEPS_PER_UNIT   { 100, 100, 400, 420 }
 
 /**
  * Default Max Feed Rate (mm/s)
