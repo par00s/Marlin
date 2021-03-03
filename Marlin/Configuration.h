0daf2ea91cdeccee42dd72736fe49da0a2f95585
Piotr Sikora <psikora@inelo.pl>
2021-03-03 15:34:16 +0100

    Calibrated value of X and Y axis stepps/mm DEFAULT_AXIS_STEPS_PER_UNIT   { 204, 204, 400, 420 }

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -740,7 +740,7 @@
  * Override with M92
  *                                      X, Y, Z, E0 [, E1[, E2...]]
  */
-#define DEFAULT_AXIS_STEPS_PER_UNIT   { 200, 200, 400, 420 }
+#define DEFAULT_AXIS_STEPS_PER_UNIT   { 204, 204, 400, 420 }
 
 /**
  * Default Max Feed Rate (mm/s)
